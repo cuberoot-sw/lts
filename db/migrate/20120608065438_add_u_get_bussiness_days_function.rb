d5d852d05e28ba3280530f5c408d88f192f8dcb8
Mayur Khatri <mayur@Mayurs-Mac-mini.local>
2012-10-09 14:42:04 +0530

    migration to postgresql

--- db/migrate/20120608065438_add_u_get_bussiness_days_function.rb
+++ db/migrate/20120608065438_add_u_get_bussiness_days_function.rb
@@ -4,36 +4,38 @@ class AddUGetBussinessDaysFunction < ActiveRecord::Migration
       when ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
           connection.execute(%q{
           CREATE function uGetBussinessDays(in_sDate DATE,
-           in_eDate DATE) RETURNS VARCHAR(100)
-          BEGIN
-           DECLARE l_count INT;
-           DECLARE l_sDate DATE;
-           SET l_sDate = in_sDate;
+           in_eDate DATE) RETURNS VARCHAR(100) AS $$
+           DECLARE
+            l_count INT;
+            l_sDate DATE;
 
-           CREATE TEMPORARY TABLE IF NOT EXISTS _tblBussinessDays\
-              (BussinessDays date);
-           CREATE TEMPORARY TABLE IF NOT EXISTS _tblHolidaysDays(Offdays date);
+           BEGIN
+            SET l_sDate = in_sDate;
 
-            delete from _tblBussinessDays;
-            delete from _tblHolidaysDays;
+            CREATE TEMPORARY TABLE IF NOT EXISTS _tblBussinessDays(BussinessDays date);
+            CREATE TEMPORARY TABLE IF NOT EXISTS _tblHolidaysDays(Offdays date);
 
-            WHILE l_sDate <= in_eDate DO
+             delete from _tblBussinessDays;
+             delete from _tblHolidaysDays;
 
-             IF (DAYNAME(l_sDate) = 'Sunday' ) THEN
-                INSERT INTO _tblHolidaysDays VALUES(l_sDate);
-             ELSEIF ( DAYNAME(l_sDate) = 'Saturday' ) THEN
-                INSERT INTO _tblHolidaysDays VALUES(l_sDate);
+             WHILE l_sDate <= in_eDate LOOP
+
+              IF (DAYNAME(l_sDate) = 'Sunday' ) THEN
+                 INSERT INTO _tblHolidaysDays VALUES(l_sDate);
+              ELSIF ( DAYNAME(l_sDate) = 'Saturday' ) THEN
+                 INSERT INTO _tblHolidaysDays VALUES(l_sDate);
               ELSE
-                INSERT INTO _tblBussinessDays VALUES(l_sDate);
+                 INSERT INTO _tblBussinessDays VALUES(l_sDate);
               END IF;
 
-              SET l_sDate = DATE_ADD(l_sDate,INTERVAL 1 DAY);
+               l_sDate = l_sDate + "1 day";
 
-            END WHILE;
+             END LOOP;
 
-            SELECT count(BussinessDays) INTO l_count FROM _tblBussinessDays;
-            return l_count ;
-          END
+             SELECT count(BussinessDays) INTO l_count FROM _tblBussinessDays;
+             return l_count ;
+           END;
+           $$ language plpgsql;
           })
       when ActiveRecord::ConnectionAdapters::MySQLAdapter
         execute "
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120607131640) do
+ActiveRecord::Schema.define(:version => 20120608065438) do
 
   create_table "holidays", :force => true do |t|
     t.datetime "date"
